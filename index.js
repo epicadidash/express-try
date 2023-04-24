b2e33f3971e37e3b789c48ba862aacc1bd62744a
epicadidash <dradwaitdash@gmail.com>
2023-04-24 13:59:41 +0530

    New routes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     console.log(req.ip)
   res.redirect(301,'https://github.com/epicadidash')
 })
+app.get('/h', (req, res) => {
+    console.log(req.ip)
+  res.send('hello world')
+})
 
 app.listen(port, () => {
   console.log(`Server started  ${port}`)
